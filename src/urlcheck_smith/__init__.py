93a652dfa73f024fc62c4edd7f8a6ac6696f1de8
Eiichi YAMAMOTO <yeiichi@mac.com>
2026-01-22 10:11:45 +0900

    chore: add src/urlcheck_smith/__init__.py

--- src/urlcheck_smith/__init__.py
+++ src/urlcheck_smith/__init__.py
@@ -1,6 +1,15 @@
-# src/urlcheck_smith/data/__init__.py
-"""
-Data package for urlcheck-smith.
+from __future__ import annotations
 
-Contains YAML files for classification rules and presets.
-"""
+from .check import check_urls
+from .classify import SiteClassifier
+from .extract import extract_urls_from_text, extract_urls_from_file, extract_urls_from_paths
+from .models import UrlRecord
+
+__all__ = [
+    "check_urls",
+    "SiteClassifier",
+    "extract_urls_from_text",
+    "extract_urls_from_file",
+    "extract_urls_from_paths",
+    "UrlRecord",
+]
